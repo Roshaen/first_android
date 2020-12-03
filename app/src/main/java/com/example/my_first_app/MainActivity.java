be140425f4f5d0f7cbc0e771619dbb1c88cd93b7
Roshan Kumar <roshaen09@gmail.com>
2020-12-03 18:30:23 +0530

    Added a toast on login button

--- app/src/main/java/com/example/my_first_app/MainActivity.java
+++ app/src/main/java/com/example/my_first_app/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.my_first_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void login(View view){
+
+        Toast.makeText(getApplicationContext(), "No account found", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
